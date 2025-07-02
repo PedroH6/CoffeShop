06c927fea2d1763ecbb7381a984b537c49f104bb
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-07-02 14:14:00 -0300

    criando o components customButton

---
+++ lib/components/custom_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final double? width;
+  final double? height;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final double? borderRadius;
+  final double? elevation;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.width,
+    required this.height,
+    required this.fontSize,
+    required this.fontWeight,
+    required this.borderRadius,
+    required this.elevation,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width ?? double.infinity,
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor ?? const Color(0xFFD2691E),
+          foregroundColor: textColor ?? Colors.white,
+          elevation: elevation ?? 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius ?? 16)
+          )
+        ), 
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+          ),
+        ),
+        ),
+    );
+  }
+}
